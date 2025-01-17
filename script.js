9f5b41e97ac77dd09abe9592413c8b0910131ee8
1206Elchi <elchi.der.zweite@gmail.com>
2025-01-17 03:23:00 +0100

    Add Website
    Already prepared it in VScode

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Willkommen auf dem WZD Community Minecraft Server!');
+
+    // Hier können Sie in Zukunft weitere Funktionalitäten hinzufügen
+});
